9f76c11d1489d1194375ad656f0953c6a680ab7e
loyangliu <loyangliu@gmail.com>
2018-04-24 05:18:50 -0700

    去掉文件缓存

--- libs/cache/cachecfg.php
+++ libs/cache/cachecfg.php
@@ -4,12 +4,6 @@
 class CACHE_CONFIG
 {
 	public static $caches = array(
-			'file' => array(
-					'type' => 'file',
-					'classname' => 'FileCache',
-					'filename' => 'file.cache'
-			),
-			
 			'db' => array(
 					'type' => 'db',
 					'classname' => 'DbCache',
