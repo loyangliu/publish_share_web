c94c2c4b91bba4ce642c1574b58e8cb5ec20200b
loyangliu <loyangliu@gmail.com>
2018-05-05 22:35:48 -0700

    增加API验证

--- app/api/auth/class/auth.controller.php
+++ app/api/auth/class/auth.controller.php
@@ -2,8 +2,26 @@
 
 class AuthController extends AppController
 {
-    public function login()
-    {
+	public function loginCheck() {
+		return true;
+	}
+	
+	// 登录态检查
+	public function check() {
+		$api_token = addslashes($_REQUEST['api_token']);
+		if($api_token){
+			$user = $this->model->db->getRow("select * from users where api_token='{$api_token}'");
+			if($user){
+				echo apiJson(0, '已经认证', ['user' => $user]);
+				return;
+			}
+		}
+		
+		echo apiJson(1000, '未认证');
+	}
+	
+	// 登录
+    public function login() {
         $data = array_map(function($v){
             return addslashes(trim($v));
         }, $_POST);
